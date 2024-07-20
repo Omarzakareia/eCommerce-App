8c50062195a3c74ed968a20bb8a3a47cf254cf2b
Omar <omarzakareia868@gmail.com>
2024-07-20 21:03:09 +0300

    "Edit Swagger"

--- Talabat.APIs/Extensions/SwaggerServicesExtensions.cs
+++ Talabat.APIs/Extensions/SwaggerServicesExtensions.cs
@@ -4,12 +4,51 @@ namespace Talabat.APIs.Extensions
 {
     public static class SwaggerServicesExtensions
     {
-        public static IServiceCollection AddSwaggerServices(this IServiceCollection Services)
+        public static IServiceCollection AddSwaggerServices(this IServiceCollection services)
         {
-            Services.AddEndpointsApiExplorer();
-            Services.AddSwaggerGen();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Talabat API",
+                    Description = "An API for Talabat E-Commerce",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Omar Zakareia",
+                        Email = "omarzakareia868@gmail.com",
+                        Url = new Uri("https://example.com/contact"),
+                    },
 
-            return Services;
+                });
+
+                // Add security definition for JWT Bearer token authentication
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                }});
+            });
+
+            return services;
         }
     }
 }
